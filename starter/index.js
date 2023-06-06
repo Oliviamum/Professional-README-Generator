f40130311205a0cbacf916f8d0d29330e14960b9
Amaka Metu <103032546+Oliviamum@users.noreply.github.com>
2023-06-06 21:08:56 +0100

    Add files via upload

---
+++ starter/index.js
@@ -0,0 +1,78 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "What is the title of your project?",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Enter a description for your project:",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "Enter installation instructions:",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "Enter usage information:",
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "Choose a license for your project:",
+    choices: [
+      "MIT",
+      "Apache 2.0",
+      "GPL 3.0",
+      "BSD 3-Clause",
+      "None",
+    ],
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "Enter contribution guidelines:",
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: "Enter test instructions:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "Enter your email address:",
+  },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
+}
+
+// function to initialize program
+function init() {
+  inquirer.prompt(questions).then((answers) => {
+    const fileName = "README.md";
+    const readmeData = generateMarkdown(answers);
+    writeToFile(fileName, readmeData);
+    console.log("README.md file generated successfully!");
+  });
+}
+
+// function call to initialize program
+init();
